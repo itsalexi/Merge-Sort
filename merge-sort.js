a5fa3ed03c39ead544b3b17df8b3ba3cb6be44ee
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-08-10 10:07:21 +0800

    array now splits in to two, left and right

--- merge-sort.js
+++ merge-sort.js
@@ -19,3 +19,17 @@
 // left[0] and right[0], we can use the .shift() function to remove the first element of the
 // given half so when we loop back later we can just compare the first elements again.
 // Then we return the array and we add the left and right half with it as well.
+
+function mergeSort(array) {
+    if (array.length <= 1) {
+        return array;
+    }
+
+    let middle = Math.floor(array.length / 2);
+    let left = array.slice(0, middle);
+    let right = array.slice(middle, array.length);
+
+    console.log(left, right);
+}
+
+mergeSort([1, 2, 3, 5]);
